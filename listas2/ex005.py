4fd2f38bd9d2b371d858a9ba09ab425529064d50
Hallan Victor Pereira de Almeida <hvpa@WINDOWS.CIN.UFPE.BR>
2025-10-01 08:59:40 -0300

    alteraçao

--- listas2/ex005.py
+++ listas2/ex005.py
@@ -20,10 +20,10 @@ erros_clara = 0
 
 jogador_atual = bolinhas_andre
 
-perdedor_1 = 1 
-perdedor_2 = 1
+perdedor_1 = True
+perdedor_2 = True
 
-while perdedor_1 != 0 and perdedor_2 != 0:
+while perdedor_1 != False and perdedor_2 != False:
      
     resultado_rodada = input()
 
@@ -36,45 +36,64 @@ while perdedor_1 != 0 and perdedor_2 != 0:
     elif jogador_atual == bolinhas_clara:
         segundo_jogador = bolinhas_andre
         terceiro_jogador = bolinhas_bruno
-
-    while resultado_rodada == "ACERTOU" :
+ 
+    if resultado_rodada == "acertou" :
         if jogador_atual == bolinhas_andre:
+            
             bolinhas_andre = bolinhas_andre + 2 
-            while segundo_jogador > 0:
+            acertos_andre = bolinhas_andre + 1
+            
+            if segundo_jogador > 0:
                 segundo_jogador = segundo_jogador - 1 
-            while terceiro_jogador > 0:
+            elif terceiro_jogador > 0:
                 terceiro_jogador = terceiro_jogador - 1
+            
             if segundo_jogador == 0:
-                perdedor_1 = 1
+                perdedor_1 = False
             
             if terceiro_jogador == 0:
-                perdedor_1 = 1
-
+                perdedor_1 = False
+            
+            if bolinhas_andre == jogador_atual:
+                bolinhas_bruno = jogador_atual
         elif jogador_atual == bolinhas_bruno:
+            
             bolinhas_bruno = bolinhas_bruno + 2 
-            while segundo_jogador > 0:
-                segundo_jogador = segundo_jogador - 1 
-            while terceiro_jogador > 0:
-                terceiro_jogador = terceiro_jogador - 1
-            if segundo_jogador == 0:
-                perdedor_1 = 1
-            
-            if terceiro_jogador == 0:
-                perdedor_1 = 1
-                
-        elif jogador_atual == bolinhas_bruno:
-            bolinhas_andre = bolinhas_bruno + 2 
-            while segundo_jogador > 0:
-                segundo_jogador = segundo_jogador - 1 
-            while terceiro_jogador > 0:
-                terceiro_jogador = terceiro_jogador - 1
-            if segundo_jogador == 0:
-                perdedor_1 = 1
-            
-            if terceiro_jogador == 0:
-                perdedor_1 = 1
-        
+            acertos_bruno = bolinhas_bruno + 1 
 
+            if segundo_jogador > 0:
+                segundo_jogador = segundo_jogador - 1 
+            elif terceiro_jogador > 0:
+                terceiro_jogador = terceiro_jogador - 1
+            
+            
+            if segundo_jogador == 0:
+                perdedor_1 = False
+            
+            if terceiro_jogador == 0:
+                perdedor_1 = False
+            
+            if bolinhas_bruno == jogador_atual:
+                bolinhas_clara = jogador_atual
+        elif jogador_atual == bolinhas_bruno:
+           
+            bolinhas_clara = bolinhas_clara + 2 
+            acertos_clara = acertos_clara + 1
+            
+            if segundo_jogador > 0:
+                segundo_jogador = segundo_jogador - 1 
+            elif terceiro_jogador > 0:
+                terceiro_jogador = terceiro_jogador - 1
+            
+            if segundo_jogador == 0:
+                perdedor_1 = False
+            
+            if terceiro_jogador == 0:
+                perdedor_1 = False
+            
+            if bolinhas_clara == jogador_atual:
+                bolinhas_andre = jogador_atual
+            
         else:
             if jogador_atual == bolinhas_andre:
                 erros_andre = erros_andre + 1 
@@ -84,36 +103,38 @@ while perdedor_1 != 0 and perdedor_2 != 0:
                 erros_clara = erros_clara +1
             
             if jogador_atual == bolinhas_andre:
-                while erros_andre < 3:
-                    if resultado_rodada == "ERROU":
-                        bolinhas_andre = terceiro_jogador
+                if erros_andre < 3:
+                    if resultado_rodada == "errou":
+                        bolinhas_bruno = jogador_atual
+                        
             elif jogador_atual == bolinhas_bruno:
-                while erros_bruno < 3:
-                    if resultado_rodada == "ERROU":
-                        bolinhas_bruno = terceiro_jogador
+                if erros_bruno < 3:
+                    if resultado_rodada == "errou":
+                        bolinhas_clara = jogador_atual
             elif jogador_atual == bolinhas_clara:
-                while erros_bruno < 3:
-                    if resultado_rodada == "ERROU":
-                        bolinhas_clara = terceiro_jogador
+                    if erros_bruno < 3:
+                        if resultado_rodada == "errou":
+                            bolinhas_andre = jogador_atual
             
 
             if erros_andre == 3:
-                perdedor_2 = 0
+                perdedor_2 = False
                 saiu_antes_acabar = "andre"
             elif erros_bruno == 3:
-                perdedor_2 = 0
+                perdedor_2 = False
                 saiu_antes_acabar = "bruno"
             elif erros_clara == 3:
-                perdedor_2 = 0
+                perdedor_2 = False
                 saiu_antes_acabar = "clara"
+                
+    if jogador_atual > segundo_jogador and segundo_jogador < terceiro_jogador:
+        vencedor = jogador_atual
+    elif jogador_atual < segundo_jogador and segundo_jogador > terceiro_jogador:
+        vencedor = segundo_jogador
+    else:
+        vencedor = terceiro_jogador
 
 
-
-
-            
-
-
-    
 
              
                 
